e91cde27b681f78d69505d8e3e85bd777734a4c2
danielc051 <connerydaniel@gmail.com>
2026-03-20 08:08:37 +1300

    Commit

---
+++ MinimalApi/TimerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("time")]
+public class TimeController : ControllerBase
+{
+    [HttpGet("utc")]
+    public IActionResult GetUtcTime()
+    {
+        return Ok(DateTime.UtcNow);
+    }
+}
